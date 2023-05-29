3fbd5c12028743129520f963405993680a833e1d
Alexandru-Sergiu Marton <brown121407@posteo.ro>
2023-05-29 21:09:13 +0300

    Add recoil animation and muzzle flash.

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -4,6 +4,9 @@ extends Node3D
 
 @onready var reload_timer: Timer = $ReloadTimer
 @onready var shot_timer: Timer = $ShotTimer
+@onready var fire_particles: GPUParticles3D = $Muzzle/SmokeParticles
+@onready var muzzle_flash: OmniLight3D = $Muzzle/MuzzleFlash
+@onready var light_timer: Timer = $Muzzle/LightTimer
 
 var ready_to_shoot: bool :
 	get:
@@ -25,6 +28,8 @@ var reload_progress := 0 :
 func _ready():
 	stats.current_ammo = stats.max_ammo
 	reload_timer.timeout.connect(track_reload)
+	light_timer.timeout.connect(func (): muzzle_flash.visible = false)
+	
 
 func shoot():
 	if not ready_to_shoot or stats.current_ammo <= 0 or is_reloading:
@@ -32,6 +37,13 @@ func shoot():
 
 	stats.current_ammo -= 1
 	ammo_changed.emit(stats.current_ammo)
+	
+	fire_particles.emitting = true
+	muzzle_flash.visible = true
+	light_timer.start()
+	var recoil_tween: Tween = get_tree().create_tween().bind_node(self)
+	recoil_tween.tween_property(self, 'rotation', Vector3(0.35, 0, 0), 0.02)
+	recoil_tween.tween_property(self, 'rotation', Vector3(0, 0, 0), 0.2)
 	
 	ready_to_shoot = false
 	shot_timer.start()
